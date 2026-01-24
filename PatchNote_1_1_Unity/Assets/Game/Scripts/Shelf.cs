f2a614832008dc7081e03784c4b855ce2a934194
hoimankyuen <hoiman.k.yuen@gmail.com>
2026-01-24 03:18:18 +0000

    Updated how shelf works

--- PatchNote_1_1_Unity/Assets/Game/Scripts/Shelf.cs
+++ PatchNote_1_1_Unity/Assets/Game/Scripts/Shelf.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using MoonlightTools.GizmoTools;
+using MoonlightTools.MathTools;
 using UnityEngine;
 
 public class Shelf : MonoBehaviour
@@ -6,19 +8,50 @@ public class Shelf : MonoBehaviour
     private static readonly int Collide = Animator.StringToHash("Collide");
     
     [Header("References")]
+    [SerializeField] private ItemList m_itemList;
+    
+    [Header("Components")]
     [SerializeField] private Animator m_animator;
     [SerializeField] private List<Transform> m_itemSpawnPositions;
-    [SerializeField] private ItemList m_itemList;
+    [SerializeField] private Transform m_launchDirection;
+
     
     [Header("Settings")]
     [SerializeField] private float m_forceRequiredForItemsFall = 1f;
-    [SerializeField] private float m_forceOffShelf = 5f;
-
+    //[SerializeField] private float m_forceOffShelf = 5f;
+    [SerializeField] private RangedFloat m_forceOffShelfRandomRange;
+    [SerializeField] private RangedFloat m_launchDirectionRandomRange;
+    
     private List<Rigidbody> m_items = new List<Rigidbody>();
+    
+    private bool m_launched = false;
     
     private void Start()
     {
         SetupItems();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (m_launchDirection != null)
+        {
+            Gizmos.color = Color.green;
+            GizmoUtils.DrawArrow(
+                m_launchDirection.position,
+                m_launchDirection.position + m_launchDirection.forward * 1f, 
+                0.5f, 
+                m_launchDirection.up);
+        }
+        
+        Gizmos.color = Color.yellow;
+        foreach (Transform itemSpawnPosition in m_itemSpawnPositions)
+        {
+            if (itemSpawnPosition != null)
+            {
+                Gizmos.DrawWireSphere(itemSpawnPosition.position, 0.1f);
+
+            }
+        }
     }
 
     private void SetupItems()
@@ -32,17 +65,22 @@ public class Shelf : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter(Collision other)
+    private void OnCollisionEnter(Collision collision)
     {
-        if (other.gameObject.layer != LayerMask.NameToLayer("Player")) return;
-            
-        other.gameObject.TryGetComponent(out Rigidbody rb);
+        if (m_launched)
+            return;
         
-        if (rb.linearVelocity.magnitude < m_forceRequiredForItemsFall) return;
+        if (collision.gameObject.layer != LayerMask.NameToLayer("Player")) return;
+            
+        //collision.gameObject.TryGetComponent(out Rigidbody rb);
+        
+        if (collision.relativeVelocity.magnitude < m_forceRequiredForItemsFall) return;
 
         m_animator.SetTrigger(Collide);
         
         ActivatePhysicsForItems();
+
+        m_launched = true;
     }
 
     private void ActivatePhysicsForItems()
@@ -50,7 +88,10 @@ public class Shelf : MonoBehaviour
         foreach (Rigidbody item in m_items)
         {
             item.isKinematic = false;
-            item.AddForce(item.transform.forward * m_forceOffShelf);
+            //item.AddForce(item.transform.forward * m_forceOffShelf);
+            Vector3 direction = Quaternion.AngleAxis(m_launchDirectionRandomRange.Random(), m_launchDirection.up) * m_launchDirection.forward;
+            float force = m_forceOffShelfRandomRange.Random();
+            item.AddForce(direction * force, ForceMode.Impulse);
         }
     }
 }
