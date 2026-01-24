b85ac0c8136ce930b6d140a775494de2d9af54eb
hoimankyuen <hoiman.k.yuen@gmail.com>
2026-01-24 15:51:49 +0000

    Reorganise files

---
+++ PatchNote_1_1_Unity/Assets/Game/Scripts/ItemType.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public enum ItemType
+{
+    Tomato,
+    Leek,
+    Swede,
+    Potato,
+    Broccoli,
+    
+    Beef,
+    Egg,
+    Cheese,
+    Bread,
+    Sausage,
+    Fish,
+    
+    Can,
+    Oil,
+    Pasta,
+    Bottle,
+    Cereal,
+    Milk,
+    
+    Gold,
+    Crisps,
+    Jam,
+    Ham,
+    Watermelon,
+    Gem
+}
