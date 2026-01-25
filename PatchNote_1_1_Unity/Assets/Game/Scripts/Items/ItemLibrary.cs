e7742d8760b795fceae849495b32a8e43e335229
hoimankyuen <hoiman.k.yuen@gmail.com>
2026-01-25 01:36:25 +0000

    Changed syntax

--- PatchNote_1_1_Unity/Assets/Game/Scripts/Items/ItemLibrary.cs
+++ PatchNote_1_1_Unity/Assets/Game/Scripts/Items/ItemLibrary.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Data/Item Library", fileName = "ItemLibrary")]
 public class ItemLibrary : ScriptableObject
 {
-    [SerializeField] private List<ItemData> items;
+    [SerializeField] private List<ItemData> Items;
 
     private Dictionary<ItemType, ItemData> m_runtimeItemDataLookup;
     
@@ -13,7 +13,7 @@ public class ItemLibrary : ScriptableObject
         if (m_runtimeItemDataLookup == null)
         {
             m_runtimeItemDataLookup = new Dictionary<ItemType, ItemData>();
-            foreach (ItemData itemData in items)
+            foreach (ItemData itemData in Items)
             {
                 m_runtimeItemDataLookup[itemData.Type] = itemData;
             }
